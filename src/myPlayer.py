94f4c3eb5912192ec99d4a74d275b545d71fb3dc
Achraf <bentaheraachraf@gmail.com>
2020-05-29 02:24:28 +0200

    evaluate

--- src/myPlayer.py
+++ src/myPlayer.py
@@ -55,4 +55,79 @@ class myPlayer(PlayerInterface):
             print("I lost :(!!")
 
 
+    def evaluate(self,b):
 
+        score = 0
+        # !add coef to every goal
+        # maximizing the number of stones *
+        goal1 = b._nbBLACK - b._nbWHITE
+        score += goal1 
+        #print(goal1)
+        
+        
+        # maximizing the number of liberties
+        libertyBlack = 0
+        libertyWhite = 0
+        for fcoord in range( (b._BOARDSIZE -1) * 10) :
+            if b._stringLiberties[fcoord ] != -1:
+                if b._board[ fcoord ] == 1:
+                    libertyBlack += b._stringLiberties[ fcoord ]
+                else:
+                    libertyWhite += b._stringLiberties[ fcoord ]
+        goal2 = libertyBlack - libertyWhite
+        score += goal2
+        #print(goal2)
+        # avoinding moves on the edge
+        # !goal3 value
+        goal3 = 0
+        isOnBoard = True
+        coord = b.name_to_coord( b._historyMoveNames[-1] )
+        x, y = coord
+        if( x == -1 ):
+            goal3 = 0
+        else:  
+            neighbors = ((x+1, y), (x-1, y), (x, y+1), (x, y-1))
+            for c in neighbors:
+                isOnBoard = isOnBoard and b._isOnBoard(c[0], c[1])
+            if not isOnBoard:
+                goal3 = - 10
+        
+        score += goal3
+        #print(goal3)
+        #connectiong stones
+        # dictionnaire de pairs racineString:longeur !can remove it
+        d = {}
+        f = []
+           
+        for fcoord in range( (b._BOARDSIZE-1) * 10 + 1):
+            f.append(fcoord)
+        
+        for fc in f:
+            color = self._board[fc]
+            if color == 0:
+                continue
+            string = set([fc])
+            frontier = [fc]
+            while frontier:
+                current_fc = frontier.pop() 
+                string.add(current_fc)
+                i = b._neighborsEntries[current_fc]
+                while b._neighbors[i] != -1:
+                    fn = b._neighbors[i]
+                    i += 1
+                    if b._board[fn] == color and not fn in string:
+                        frontier.append(fn)
+            #d[fc] = len(string)
+            score += len(string) * (1 + len(string)/100)
+
+            for s in string:
+                if( s != fc):
+                    f.remove(s)
+            
+        '''
+        for key,value in d.items():
+            print( key , value )
+        '''
+        return score
+       
+       
